2554902e99e47029067e621372500fe4ae5e841f
redazwm-gif <redazwm@gmail.com>
2026-02-11 03:54:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,8 +49,8 @@ intents.webhooks = True
 intents.members = True
 intents.presences = True
 
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
+# Uncomment this if you want to use prefix (normal) commands.
+# It is recommended to use slash commands and therefore not use prefix commands.
 
 If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
 
