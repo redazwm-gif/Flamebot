c0a1233248767ce5a2811eaaf2859f267cf535e6
redazwm-gif <redazwm@gmail.com>
2026-02-11 12:37:44 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('Bot đã online!');
+});
+
+client.login(process.env.TOKEN);
